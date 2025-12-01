9ddd5eb0307b8a63e2b3bfa8806e6baa5468e1ad
Prabesh Acharya <praveshach1032@gmail.com>
2025-12-01 15:29:30 +0545

    api maded react

---
+++ Laravel/app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\SignupRequest;
+use App\Models\User;
+use Illuminate\Container\Attributes\Log;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request)
+    {
+        $data = $request->validated();
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
+       $token = $user->createToken('main')->plainTextToken;
+       return response(compact('user', 'token'));
+
+    }
+    public function signup(SignupRequest $request)
+
+    {
+
+    }
+    public function logout(Request $request)
+    {
+
+    }
+}
